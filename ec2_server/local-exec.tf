b2873f7f4ba5eef6b693b43d032251580692da76
VlN9 <tron4ik.ne@gmail.com>
2022-12-20 17:00:23 +0200

    modified inventory file

--- ec2_server/local-exec.tf
+++ ec2_server/local-exec.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "add_hosts" {
   provisioner "local-exec" {
-    command = "echo \"[prod_servers]\n${module.ec2_server.aws_instanse_prvt_ip[0]}\" > ./hosts.txt"
+    command = "echo \"[prod_servers]\n${module.ec2_server.aws_instanse_prvt_ip[0]}\" > /var/lib/jenkins/workspace/hosts.txt"
   }
   depends_on = [
     module.ec2_server
